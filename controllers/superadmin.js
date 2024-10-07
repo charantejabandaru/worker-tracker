868c45ead655b9dc72c176830c7270f44ab97efd
charantejabandaru <charanteja.bandaru2003@gmail.com>
2024-10-07 11:35:12 +0530

    Defined few endpoints in superadmin

--- controllers/superadmin.js
+++ controllers/superadmin.js
@@ -1,7 +1,3 @@
-//put('/employee/:employeeId')
-//get('/employee/role/:role')
-//get('/employee/specification/:specification')
-//get('/employee-status/:status')
 //put('/site/:siteId')
 
 const { Router } = require('express');
@@ -10,11 +6,16 @@ const router = Router({ strict: true });
 
 router.post('/register', superAdminServices.register);
 router.get('/employee', superAdminServices.getEmployees);
+router.get('/employee/role/:role', superAdminServices.getEmployeeByRole);
+router.get('/employee/specification/:specification', superAdminServices.getEmployeeBySpecification);
+router.get('/employee-status/:status', superAdminServices.getEmployeeStatus);
+router.put('/employee/:employeeId', superAdminServices.updateEmployee);
 router.delete('/employee/:employeeId', superAdminServices.removeEmployee);
 router.post('/site', superAdminServices.addSite);
 router.get('/site', superAdminServices.getSites);
 router.get('/site/id/:siteId', superAdminServices.getSiteById);
 router.get('/site/location/:location', superAdminServices.getSiteByLocation);
+router.put('/site/:siteId', superAdminServices.updateSite);
 router.delete('/site/:siteId', superAdminServices.removeSite);
 router.post('/dailyrecord', superAdminServices.addDailyRecord);
 router.delete('/dailyrecord/:dailyRecordId', superAdminServices.removeDailyRecord);
