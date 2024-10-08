5ea4bce2ae34977c318d2d371d8ac4f3b2eb5b00
charantejabandaru <charanteja.bandaru2003@gmail.com>
2024-10-08 10:19:58 +0530

    defined new endpoints

--- middlewares/record-uploads.js
+++ middlewares/record-uploads.js
@@ -1,15 +1,37 @@
 const multer = require('multer');
+const fs = require('fs');
 const path = require('path');
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, path.join(__dirname, `../uploads/dailyrecords/${req.params.dailyrecordId}`));
+        cb(null, path.join(__dirname, `../uploads/dailyrecords/${new Date()}/${req.body.employeeId}`));
     },
     filename: (req, file, cb) => {
-        cb(null, file.originalname);
-        req.filename = file.originalname;
+        countFilesInFolder(folderPath)
+        .then(fileCount =>{
+            cb(null, fileCount+1);
+            req.filename = fileCount+1;
+        })
+        .catch(err => console.error(err));  
     }
 });
+
+function countFilesInFolder(folderPath) {
+    return new Promise((resolve, reject) => {
+      fs.readdir(folderPath, (err, files) => {
+        if (err) {
+          return reject('Unable to scan directory: ' + err);
+        }
+  
+        const fileCount = files.filter(file => {
+          const filePath = path.join(folderPath, file);
+          return fs.lstatSync(filePath).isFile();
+        }).length;
+  
+        resolve(fileCount);
+      });
+    });
+  }
 
 const uploadRecord = multer({ storage: storage });
 
