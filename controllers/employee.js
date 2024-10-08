7868d2f71b0de7ce70385874986f6d5b766200b0
karthik <karthikvarmaindukuri123@gmail.com>
2024-10-08 09:49:22 +0530

    bceypt and jwt are added

--- controllers/employee.js
+++ controllers/employee.js
@@ -2,9 +2,9 @@ const express = require('express');
 const employeeRouter = express.Router();
 const employeeServices = require('../services/employee');
 
+employeeRouter.post('/login', employeeServices.checkLogin);
 employeeRouter.get('/employee/:employeeId', employeeServices.getEmployeeById);
 employeeRouter.get('/employee/dailyRecord/:employeeId', employeeServices.getTasksById);
-employeeRouter.put('/employee/:employeeId', employeeServices.updateEmployee);
 employeeRouter.put('/checkin/:dailyRecordId', employeeServices.updateCheckin);
 employeeRouter.put('/checkout/:dailyRecordId', employeeServices.updateCheckout);
 employeeRouter.put('/employee/remark/:dailyRecordId', employeeServices.updateEmployeeRemark);
